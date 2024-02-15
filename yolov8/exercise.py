c1baddfebb29e5edf7c04a42d311c581895d41b6
Jay <wnwoghd22@gmail.com>
2024-02-15 17:20:21 +0900

    practice: run yolov8

---
+++ yolov8/exercise.py
@@ -0,0 +1,15 @@
+'''
+https://docs.ultralytics.com/ko/tasks/classify/#models
+'''
+
+from ultralytics import YOLO
+
+# Load a model
+# model = YOLO('yolov8n-cls.yaml')  # build a new model from YAML
+model = YOLO('yolov8n-cls.pt')  # load a pretrained model (recommended for training)
+# model = YOLO('yolov8n-cls.yaml').load('yolov8n-cls.pt')  # build from YAML and transfer weights
+
+# Train the model
+results = model.train(data='./weather-dataset', epochs=1, imgsz=64)
+
+print('train complete')
