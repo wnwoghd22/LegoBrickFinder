245f9b6e88ecf963b1ce9efdb1c977405c00b455
Jay <wnwoghd22@gmail.com>
2024-02-16 19:27:52 +0900

    practice: predict

---
+++ yolov8/main_pred.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+import os
+
+model = YOLO('./../runs/classify/train4/weights/best.pt')
+sample_folder = './brick-data/sample'
+
+sample_files = os.listdir(sample_folder)
+
+for sample_file in sample_files:
+    sample_dir = os.path.join(sample_folder, sample_file)
+    result = model(sample_dir)
