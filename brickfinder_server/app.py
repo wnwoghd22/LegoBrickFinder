950ced349c2c68d1a6187cd5b49234acfb20dd8f
Jay <wnwoghd22@gmail.com>
2024-02-23 18:20:50 +0900

    predict method: yolo

--- brickfinder_server/app.py
+++ brickfinder_server/app.py
@@ -1,4 +1,5 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
+from PIL import Image
 from yolo_model.model import predict_yolo
 
 app = Flask(__name__)
@@ -7,11 +8,11 @@ app = Flask(__name__)
 def hello():
     return 'hello'
 
-@app.route('/predict', method=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
     if request.method == 'POST':
         file = request.files['file']
-        result = predict_yolo(file)
+        img = Image.open(file.stream)
+        result = predict_yolo(img)
 
-        return result
-        
+        return jsonify(result)
--- brickfinder_server/yolo_model/model.py
+++ brickfinder_server/yolo_model/model.py
@@ -2,7 +2,17 @@ from ultralytics import YOLO
 
 import os
 
-model = YOLO('./trained/weights/best.pt')
+model = YOLO('./yolo_model/trained/weights/best.pt')
 
 def predict_yolo(image):
-    return model(image)
+    results = model(image)
+
+    probs = results[0].probs
+
+    indices = [results[0].names[i] for i in probs.top5]
+    confs = probs.top5conf
+
+    print(indices)
+    print(confs)
+
+    return list(zip(indices, confs.tolist()))
